9b9aea8ae6786519f64c80bcfaa051bc24ca9b63
Mahmut Ali ÖZKURAN <ozkuran@users.noreply.github.com>
2018-07-18 15:33:39 +0300

    Initialize

---
+++ install-worker-centos-7.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+yum update -y
+yum install nano firewalld -y
+systemctl enable firewalld
+systemctl start firewalld
+
+
+setenforce 0
+sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+firewall-cmd --permanent --add-port=10250/tcp
+firewall-cmd --permanent --add-port=10255/tcp
+firewall-cmd --permanent --add-port=30000-32767/tcp
+firewall-cmd --permanent --add-port=6783/tcp
+firewall-cmd  --reload
+echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+yum  install kubeadm docker -y
+systemctl restart docker && systemctl enable docker
